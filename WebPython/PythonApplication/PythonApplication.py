6620e21aeabc38a4f1a1b9d53a07c67c9da3cddf
cksun <1037134100@qq.com>
2018-06-22 09:22:23 +0800

    first commit

---
+++ WebPython/PythonApplication/PythonApplication.py
@@ -0,0 +1,38 @@
+
+#自定义函数
+#def power(x,n):
+#    s = 1
+#         while n > 0:
+#             n = n - 1
+#             s = s * x 
+#             return s
+
+#print(power(23,2));
+
+#def power(x):
+#    return x*x;
+#print (power(5));
+
+#age = 3
+#if age >= 18:
+#    print('your age is', age)
+#    print('adult')
+#else:
+#    print('your age is', age)
+#    print('teenager')
+
+#age = 3
+#if age >= 18:
+#    print('adult')
+#elif age >= 6:
+#    print('teenager')
+#else:
+#    print('kid')
+
+sum = 0
+for x in [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]:
+    sum = sum + x
+print(sum)
+
+
+
